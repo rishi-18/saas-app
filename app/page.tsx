a698103e4fe6b0a096770842106dddfa518131d7
Rishi <define.rishi18@gmail.com>
2025-06-08 23:37:42 +0530

    home page pt 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,46 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>hello rishi, it's your saas app</h1>
-      <Button>Let's get started</Button>
-    </div>
+    <main>
+      <h1 className='text-2xl'>Popular Companions</h1>
+      
+      <section className='home-section'>
+        <CompanionCard
+          id="123"
+          name="Neura the Explorer"
+          topic="Neural Networks"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="113"
+          name="Geura the Mapper"
+          topic="Neural Networks and AI"
+          subject="Tech"
+          duration={35}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="133"
+          name="Qura the Language"
+          topic="Networks"
+          subject="English"
+          duration={55}
+          color="#afea6e"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList/>
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
---
+++ components/CompanionsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompanionsList = () => {
+  return (
+    <div>
+      companions list
+    </div>
+  )
+}
+
+export default CompanionsList
