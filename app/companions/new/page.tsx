a124ee78dd06b332eeaefdeaee3da77692d7eecb
Rishi <define.rishi18@gmail.com>
2025-06-21 16:33:38 +0530

    final commit

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,9 @@
 import { redirect } from "next/navigation";
 import CompanionForm from "@/components/CompanionForm"
 import { auth } from "@clerk/nextjs/server";
+import { newCompanionPermissions } from "@/lib/actions/companion.actions";
+import Image from "next/image";
+import Link from "next/link";
 
 const NewCompanion = async () => {
 
@@ -9,12 +12,27 @@ const NewCompanion = async () => {
     redirect("/sign-in");
   }
 
+  const canCreateCompanion = await newCompanionPermissions();
+
   return (
+    
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-      <article className="w-full gap-4 flex flex-col">
+      {canCreateCompanion ? (<article className="w-full gap-4 flex flex-col">
         <h1>Companion Builder</h1>
         <CompanionForm/>
-      </article>
+      </article>): (
+        <article className="companion-limit">
+          <Image src="/images/limit.svg" alt="Companion limit reached" width={360} height={230} />
+                        <div className="cta-badge">
+                            Upgrade your plan
+                        </div>
+                        <h1>You’ve Reached Your Limit</h1>
+                        <p>You’ve reached your companion limit. Upgrade to create more companions and premium features.</p>
+                        <Link href="/Subscription" className="btn-primary w-full justify-center" >
+                            Upgrade My Plan
+                        </Link>
+                        </article>
+      )}
     </main>
   )
 }
